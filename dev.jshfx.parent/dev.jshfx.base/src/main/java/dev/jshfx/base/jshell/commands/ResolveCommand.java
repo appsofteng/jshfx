3d61c5a5aef53a80355ca157aec3fb68974e7612
appsofteng <19367016+appsofteng@users.noreply.github.com>
2021-10-22 23:11:28 +0200

    Updated the resolve command

--- dev.jshfx.parent/dev.jshfx.base/src/main/java/dev/jshfx/base/jshell/commands/ResolveCommand.java
+++ dev.jshfx.parent/dev.jshfx.base/src/main/java/dev/jshfx/base/jshell/commands/ResolveCommand.java
@@ -1,10 +1,12 @@
 package dev.jshfx.base.jshell.commands;
 
+import java.nio.file.Path;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
 import dev.jshfx.base.jshell.CommandProcessor;
+import dev.jshfx.base.sys.PreferenceManager;
 import dev.jshfx.base.sys.RepositoryManager;
 import dev.jshfx.jfx.util.FXResourceBundle;
 import picocli.CommandLine.Command;
@@ -34,7 +36,12 @@ public class ResolveCommand extends BaseCommand {
 
                 for (String coord : coords) {
                     if (coord.endsWith(".xml")) {
-                        RepositoryManager.get().resolvePom(coord, artifacts);
+                        Path path = Path.of(coord);
+                        if (!path.isAbsolute()) {
+                            path = PreferenceManager.get().getLatestDir().resolve(path);
+                        }
+                        
+                        RepositoryManager.get().resolvePom(path.toString(), artifacts);
                     } else {
                         RepositoryManager.get().resolve(coord, artifacts);
                     }
