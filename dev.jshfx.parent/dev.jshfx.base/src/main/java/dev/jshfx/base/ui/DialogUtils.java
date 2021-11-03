ac8a8c8d6adafaeb1a334708214874b9f72801f3
appsofteng <19367016+appsofteng@users.noreply.github.com>
2021-11-03 22:11:06 +0100

    Charts draft.

--- dev.jshfx.parent/dev.jshfx.base/src/main/java/dev/jshfx/base/ui/DialogUtils.java
+++ dev.jshfx.parent/dev.jshfx.base/src/main/java/dev/jshfx/base/ui/DialogUtils.java
@@ -24,7 +24,7 @@ public final class DialogUtils {
             Dialog<Void> dialog = new Dialog<>();
             dialog.initOwner(window);
             TilePane tilePane = new TilePane();
-            tilePane.setPrefColumns(chartsFX.size() > 1 ? 2 : 1);
+            tilePane.setPrefColumns(charts.getColumns());
             tilePane.getChildren().addAll(chartsFX);
 
             ScrollPane scrollPane = new ScrollPane(tilePane);
--- dev.jshfx.parent/dev.jshfx.util/src/main/java/dev/jshfx/util/chart/Charts.java
+++ dev.jshfx.parent/dev.jshfx.util/src/main/java/dev/jshfx/util/chart/Charts.java
@@ -5,10 +5,16 @@ import java.util.List;
 
 public final class Charts {
 
+    private int columns;
+    
     private List<Chart> charts;
     
-    private Charts (Chart... charts) {
+    private Charts (int columns, Chart... charts) {
         this.charts = Arrays.asList(charts);
+    }
+    
+    public int getColumns() {
+        return columns;
     }
     
     public List<Chart> getCharts() {
@@ -16,8 +22,13 @@ public final class Charts {
     }
     
     public static Charts show(Chart... charts) {
+        var columns = charts.length > 1 ? 2 : 1;
+        return show(columns, charts);
+    }
+    
+    public static Charts show(int columns, Chart... charts) {        
         
-        Charts result = new Charts(charts);
+        Charts result = new Charts(columns, charts);
         
         return result;
     }
