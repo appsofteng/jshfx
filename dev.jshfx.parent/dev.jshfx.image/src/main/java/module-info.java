acd17a49a6ca85ddfffc5fea03e96b0364c1b0bd
appsofteng <19367016+appsofteng@users.noreply.github.com>
2021-11-06 20:28:40 +0100

    Image module info.

---
+++ dev.jshfx.parent/dev.jshfx.image/src/main/java/module-info.java
@@ -0,0 +1,21 @@
+module dev.jfxde.image {
+
+    requires java.compiler;
+    requires java.desktop;
+    requires java.logging;
+    requires java.management;
+    requires java.naming;
+    requires java.xml;
+    requires java.scripting;
+    requires java.sql;
+
+    requires jdk.compiler;
+    requires jdk.crypto.ec;
+    requires jdk.jsobject;
+    requires jdk.net;
+    requires jdk.jshell;
+    requires jdk.management;
+    requires jdk.unsupported;
+    requires jdk.xml.dom;
+    requires jdk.zipfs;
+}
