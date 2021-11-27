ad5680bdca88d5cd09fd48b753f0977eb4955f89
appsofteng <19367016+appsofteng@users.noreply.github.com>
2021-11-27 15:20:32 +0100

    Changed Tables API.

--- dev.jshfx.parent/dev.jshfx.util/src/main/java/dev/jshfx/util/control/Tables.java
+++ dev.jshfx.parent/dev.jshfx.util/src/main/java/dev/jshfx/util/control/Tables.java
@@ -1,8 +1,10 @@
 package dev.jshfx.util.control;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
+import java.util.TreeMap;
 
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
@@ -21,22 +23,36 @@ public final class Tables {
     }
 
     @SuppressWarnings("rawtypes")
-    public static <T> TableView<Map> getTable(List<Map<String, T>> data) {
+    public static TableView<Map> getTable(List<Object[]> data) {
         TableView<Map> tableView = new TableView<>();
         var columns = data.get(0);
+        var values = data.get(1);
 
-        columns.keySet().forEach(k -> {
-            TableColumn<Map, T> column = new TableColumn<>(k);
-            column.setCellValueFactory(new MapValueFactory<>(k));
+        for (int i = 0; i < columns.length; i++) {
+        
+            TableColumn<Map, Object> column = new TableColumn<>(columns[i].toString());
+            column.setCellValueFactory(new MapValueFactory<>(columns[i].toString()));
 
-            if (columns.get(k) instanceof Number) {
+            if (values[i] instanceof Number) {
                 column.setStyle("-fx-alignment: CENTER-RIGHT;");
             }
 
             tableView.getColumns().add(column);
-        });
+        }
+        
+        List<Map<String,Object>> maps = new ArrayList<>();
+        
+        for (int i = 1; i < data.size(); i++) {
+            Map<String,Object> map = new TreeMap<>();
+            
+            for (int j = 0; j < columns.length; j++) {
+                map.put(columns[j].toString(), data.get(i)[j]);
+            }
+            
+            maps.add(map);
+        }
 
-        tableView.getItems().addAll(data);
+        tableView.getItems().addAll(maps);
 
         return tableView;
     }
